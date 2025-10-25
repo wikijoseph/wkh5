9a92f2b5a766fe2bf468bf4c8a8103e542fabee0
wikijoseph <wikijoseph8@gmail.com>
2025-10-25 10:37:56 +0800

    u

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+  base: "/wkh5/",
   plugins: [vue()],
   server: {
-    host: '0.0.0.0',
-    port: 3000
-  }
-})
+    host: "0.0.0.0",
+    port: 3000,
+  },
+});
