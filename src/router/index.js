6c020d2ae037d80995c343a4754a9c6cc8107a0b
wikijoseph <wikijoseph8@gmail.com>
2025-10-25 10:42:09 +0800

    u

--- src/router/index.js
+++ src/router/index.js
@@ -1,66 +1,66 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import { useAuthStore } from '../stores/auth'
-import Login from '../views/Login.vue'
-import Home from '../views/Home.vue'
-import WalletDetail from '../views/WalletDetail.vue'
-import Settings from '../views/Settings.vue'
-import Demo from '../views/Demo.vue'
-import Transactions from '../views/Transactions.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import { useAuthStore } from "../stores/auth";
+import Login from "../views/Login.vue";
+import Home from "../views/Home.vue";
+import WalletDetail from "../views/WalletDetail.vue";
+import Settings from "../views/Settings.vue";
+import Demo from "../views/Demo.vue";
+import Transactions from "../views/Transactions.vue";
 
 const routes = [
   {
-    path: '/login',
-    name: 'Login',
-    component: Login
+    path: "/login",
+    name: "Login",
+    component: Login,
   },
   {
-    path: '/demo',
-    name: 'Demo',
-    component: Demo
+    path: "/demo",
+    name: "Demo",
+    component: Demo,
   },
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home,
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true },
   },
   {
-    path: '/wallet/:id',
-    name: 'WalletDetail',
+    path: "/wallet/:id",
+    name: "WalletDetail",
     component: WalletDetail,
     props: true,
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true },
   },
   {
-    path: '/transactions',
-    name: 'Transactions',
+    path: "/transactions",
+    name: "Transactions",
     component: Transactions,
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true },
   },
   {
-    path: '/settings',
-    name: 'Settings',
+    path: "/settings",
+    name: "Settings",
     component: Settings,
-    meta: { requiresAuth: true }
-  }
-]
+    meta: { requiresAuth: true },
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
-})
+  history: createWebHistory("/wkh5/"),
+  routes,
+});
 
 // 路由守卫
 router.beforeEach((to, from, next) => {
-  const authStore = useAuthStore()
-  
-  if (to.meta.requiresAuth && !authStore.isLoggedIn) {
-    next('/login')
-  } else if (to.path === '/login' && authStore.isLoggedIn) {
-    next('/')
-  } else {
-    next()
-  }
-})
+  const authStore = useAuthStore();
 
-export default router
+  if (to.meta.requiresAuth && !authStore.isLoggedIn) {
+    next("/login");
+  } else if (to.path === "/login" && authStore.isLoggedIn) {
+    next("/");
+  } else {
+    next();
+  }
+});
+
+export default router;
